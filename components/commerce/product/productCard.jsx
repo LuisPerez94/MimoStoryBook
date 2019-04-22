0495fc1ee4f5fb2182f48c1734aef5879de7ad72
Luis <ingluisperez.m@outlook.com>
2019-04-22 13:36:56 -0500

    prueba de componente de producto

---
+++ components/commerce/product/productCard.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './../../../css/commerce/product/productCard.css';
+
+class ProductCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() {
+    return (
+      <div className="product-card">Producto</div>
+    );
+  }
+}
+
+export default ProductCard;
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,19 +1,15 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+// import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Welcome } from '@storybook/react/demo';
+import ProductCard from './../components/commerce/product/productCard';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+
+storiesOf('Product Card', module)
+  .add('with no product', () => <ProductCard />)
+  .add('with product', () => <ProductCard />);
